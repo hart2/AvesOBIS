05c788d373ba6dd409e4dff857a5cf7043c55354
hart2 <shartman2@usf.edu>
2020-08-10 13:35:43 -0400

    c2

--- scripts/updatedNormEffort.R
+++ scripts/updatedNormEffort.R
@@ -8,27 +8,27 @@
 # USGS Patuxent Wildlife Research Center Seabirds Compendium (collection of 50 datasets)
 
 # manipulated dataset "seabird_data_archive_2" from "seabird_data_archive_NODC_30Dec2013)
-# by adding back years and scientific names, simplifying long and lat to 4 decimal 
-# places (trying to match Aves_EEZ), adding effort
+# by adding back years and scientific names, simplifying long and lat to 4 decimal, adding effort
+
+# upload seabird_data.csv and usgs_eaec.csv into R (done previously)
 
 library(tidyverse)
+library(dplyr)
 
-usgs <- usgs_eaec7873_a1d8_43cf_baa2_27b2772a8d1f %>%  # NEED TO INCLUDE EFFORT AFTER SEABIRD AND USGS MERGED
+# rename to usgs for simplification, remove black scientificName
+usgs <- usgs_eaec7873_a1d8_43cf_baa2_27b2772a8d1f %>%
   filter(!(scientificName == "")) %>% 
   select(scientificName,eventDate,decimalLongitude,decimalLatitude,basisOfRecord,date_year,  
          individualCount, identifiedBy, institutionCode, ownerInstitutionCode, collectionCode, catalogNumber, 
          occurrenceStatus, samplingEffort)%>%  
   filter(basisOfRecord == "HumanObservation", date_year >= 1960 & date_year < 2019, decimalLatitude > -55)
 
-rm(usgs_eaec7873_a1d8_43cf_baa2_27b2772a8d1f)
-
+# Finding species present: 1960-2020 (originally wrote in AvesOBISscript.R)
+# Removing scientificName that is genus only
 genspUSGS <- usgs %>% 
   select(scientificName)                    # Create data frame with only scientific names
 freqUSGS <- as.data.frame(table(genspUSGS)) # Create a data frame with species names and how often they appear in dataset "Aves_EEZ"
 num_gsUSGS <- count(freqUSGS)               # Counts the number of genus/genus species found in dataset "freq"
-
-# Finding species present: 1960-2020-------------------------------------------------
-# (n = # of species), using a splitstring function (originally wrote in AvesOBISscript.R)
 
 v1 <- genspUSGS                      # vector with scientificNames
 v2 <- 1:nrow(genspUSGS)              # number of cells in scientificName and creating vector with the number of cells necessary for running splitstring fxn
@@ -57,32 +57,33 @@ df2 <- as.data.frame(df1) %>%
 usgs <- df2 %>% 
   select(-species)
 
-library(tidyverse)
+# making sure usgs has columns needed, only 4 decimal places for lat and lon
+usgs <- usgs %>% 
+  select(date_year,eventDate,scientificName,decimalLongitude,decimalLatitude)
+options(digits = 6)
+usgs$effort <- NA
 
-# upload seabird_data.csv and usgs_eaec.csv into R (done previously)
-x2 <- seabird_data_archive_2 %>% 
-  select(year,scientific_nm,observation_longitude,observation_latitude,effort) 
-colnames(x2) <- c("date_year","scientificName","decimalLongitude","decimalLatitude","effort")
-x1 <- usgs_eaec7873_a1d8_43cf_baa2_27b2772a8d1f %>% 
-  select(date_year,scientificName,decimalLongitude,decimalLatitude)
-
-
-library(dplyr)
-df <- left_join(x1, x2) %>%
-  # mutate(sampEffort = ifelse(gdp < break.1, "lo", 
-  #                            ifelse(gdp >= break.1 & gdp < break.2, "mid.lo",
-  #                                   ifelse(gdp >= break.2 & gdp < break.3, "mid.hi", 
-  #                                          ifelse(gdp >= break.3, "hi", NA))))) %>%
-  # arrange(country, year) %>%
-  # select(-break.1, -break.2, -break.3)
+seabird <- seabird_data_archive_2 %>% 
+  select(year,observation_date,scientific_nm,observation_longitude,observation_latitude,effort) 
+# rename columns to match usgs
+# effort is distance / hr
+colnames(seabird) <- c("date_year","eventDate","scientificName","decimalLongitude","decimalLatitude","effort")
+seabird <- seabird %>%  
+  # remove scientificName blanks
+  filter(!(scientificName == "")) %>% 
+  filter(date_year >= 1960 & date_year < 2019, decimalLatitude > -55)
 
 
+df <- left_join(usgs, seabird)
+df <- invisible(unique(df))
+
+rm(df1,df2,freqUSGS,genspUSGS,num_gsUSGS,v1,species)
 # Find species per year (from seabird_data_archive_2 put it into usgs data frame)
 
-usgsNorm <- usgs %>% 
-  filter(year == "1978") %>% 
-  select(scientificName,effort) %>% 
-  arrange(`scientific_nm`)
-
+# usgsNorm <- usgs %>% 
+#   filter(year == "1978") %>% 
+#   select(scientificName,effort) %>% 
+#   arrange(`scientific_nm`)
+# 
 # add a column of averaged effort for a species for a year
-usgsNorm <- aggregate(.~scientific_nm, FUN=mean, data=usgsNorm[, -3])
+# usgsNorm <- aggregate(.~scientific_nm, FUN=mean, data=usgsNorm[, -3])
