3a76a94dd8308cb04751604122503f08cc5d4a4e
hart2 <shartman2@usf.edu>
2022-09-09 10:23:05 -0400

    satellite data url

--- LatShift.R
+++ LatShift.R
@@ -1,9 +1,13 @@
+# Code written by Savannah Hartman, August 2022
+
 # Latitudinal shift in Atlantic and Pacific Oceans
 # Using centroid points see what environmental data is causing shift
-#   Possibly use vegan package? constrained ordination model -> canonical correspondence analysis
+#   Use UTM coordinates, Use vegan package, classical rda
 # Need environmental data for the specific points in time (Atlantic -> 
-#   care about environmental parameters for the GUlf Stream and Northern Atlantic 
-#   (SST, SSH, Surf Current, RTG: https://polar.ncep.noaa.gov/global/monitor/), 
+#   care about environmental parameters for the Gulf Stream and Northern Atlantic 
+#   (SST, SSH, Surf Current, RTG: https://polar.ncep.noaa.gov/global/monitor/),
+#   (https://coastwatch.noaa.gov/cw/satellite-data-products.html) SST, SSH, 
+#   Ocean color, Sea surface Winds
 #   Pacific -> northern shift due to temperature)
 
 # input table for Phoebastria nigripes, Puffinus griseus, Gavia immer
